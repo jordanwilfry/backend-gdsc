43893feb960c644713936d884cd5718536e349b3
Mbah  naoussi jordan wilfry <88889933+jordanwilfry@users.noreply.github.com>
2023-02-17 21:56:11 +0100

    final modification in respond to the mongodb server error

--- mongoConnect.js
+++ mongoConnect.js
@@ -7,9 +7,10 @@ module.exports = async function connection(){
             useUnifiedTopology: true
         }
 
-        const connection_string = "mongodb://localhost:27017/GDSC" || process.env.MONGODB_URI;
+        const connection_string = "mongodb://127.0.0.1:27017/GDSC"
 
-        mongoose.connect(connection_string, connection_params);
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(connection_string, connection_params);
         console.log("Connected successfully to MongoDB")
     } catch (error) {
         console.log(error)
