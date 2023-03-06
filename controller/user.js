d73c6786234ba5f2f5c784ef7f806c676170242b
Mbah  naoussi jordan wilfry <88889933+jordanwilfry@users.noreply.github.com>
2023-03-06 11:56:34 +0100

    email dependencies installed

--- controller/user.js
+++ controller/user.js
@@ -19,21 +19,32 @@ const CreateUser = async (req, res) => {
   }
 };
 
-const UpdateUser = async (req, res) => { 
-    if(req.body.password){
-      try {
-        req.body.password = await bcrypt.hash(req.body.password, 10)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
+const UpdateUser = async (req, res) => {
+  if (req.body.password) {
     try {
-      findUser = await User.findByIdAndUpdate(req.params.id, {})
+      req.body.password = await bcrypt.hash(req.body.password, 10);
     } catch (error) {
-      res.status(500).json(error)
+      console.log(error);
     }
+  }
 
-}
+  try {
+    await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json("User Updated");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-module.exports = { CreateUser };
+const DeleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User Deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { CreateUser, UpdateUser, DeleteUser};
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user");
 
-router.post("/", userController.CreateUser);
+router.post("/", userController.CreateUser);  // create user
+
+router.put("/:id", userController.UpdateUser); // update user
 
 
 module.exports = router;
